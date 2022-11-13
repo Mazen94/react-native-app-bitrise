b34ce958485a968cd5e5e4fa25871f947b8d3103
ELAYEB Mazen <melayeb@anypli.com>
2022-11-13 13:21:31 +0100

    fix tests

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,11 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
-import App from '../App';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+import App from '../App';
+
+describe('[Component] Separator', () => {
+  it('should render correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
